b75d90f8d230d5f51f19f67faf3b4a775048d6cc
RomanOt <ro@rs-site.ru>
2018-07-09 01:16:17 +0500

    hw

--- app/src/main/java/com/example/vatok/androidweather/MainActivity.java
+++ app/src/main/java/com/example/vatok/androidweather/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.vatok.androidweather;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView greetingTextView = findViewById(R.id.greeting);
+        Greeting greeting = new Greeting(this);
+        greetingTextView.setText( greeting.getText() );
     }
 }
