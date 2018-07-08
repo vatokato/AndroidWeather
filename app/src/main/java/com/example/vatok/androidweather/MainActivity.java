1fc65bf5b285d72be033889b46b825e29498eab1
RomanOt <ro@rs-site.ru>
2018-07-08 22:07:17 +0500

    test1

--- app/src/main/java/com/example/vatok/androidweather/MainActivity.java
+++ app/src/main/java/com/example/vatok/androidweather/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.vatok.androidweather;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+//изменение 1
 public class MainActivity extends AppCompatActivity {
 
     @Override
